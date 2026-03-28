9efe6d1fb188ddeec3d4d26820994ef9d5049e90
Nabaya Nestroy <nestroynabaya@gmail.com>
2026-03-28 19:58:58 +0300

    first commit

--- app/src/main/java/com/example/idcards/MainActivity.kt
+++ app/src/main/java/com/example/idcards/MainActivity.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.res.stringResource
 import com.example.idcards.ui.theme.IdCardsTheme
 import com.example.idcards.model.Student
 
